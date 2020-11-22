bf2e3fbcecce7ebb79fe3e10aac4509d2786222d
ImraS802 <68655356+ImraS802@users.noreply.github.com>
2020-11-22 22:59:57 +0100

    Added alert when button is clicked

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 function clickButton () {
-    let purpose = prompt("Why are you visiting this page - for work or for private reasons?");
-    let workStatus = prompt("Are you an employee or an employer?");
-    if (purpose === "work" && workStatus === "employer") {
-            alert("This is just an example of a web page.");
-        } else if (purpose !== "work" || workStatus !== "employer") {
-            alert("Take a look around!");
-        }
-    let toDoList = prompt("Do you want to take a glimpse on my to-do-list?");
-    if (toDoList === "Yes") {
-    alert(toDoList + "? Then welcome!");
-    } else if (toDoList === "No") {
-        alert(toDoList + "? Then please leave this page!");
-    } else {
-    alert("Sorry, this is not a valid answer. Please type 'Yes' or 'No' instead. Make sure to use an uppercase letter in the beginning.");
+    let name = prompt("What is your name?");
+    let email = prompt("What is your email address?");
+    alert("Thank you for subscribing " + name +"!")
     }
-    }
+
+    let newsletterButton = document.querySelector("button");
+    newsletterButton.addEventListener("click", clickButton);
     
-    clickButton();
