7a4ae73c6c2eff728ab10df3fda10d09d319de4e
ImraS802 <68655356+ImraS802@users.noreply.github.com>
2020-11-26 01:43:37 +0100

    Changed alert

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function updateHeading(newHeading) {
+    let heading = document.querySelector("h1");
+    heading.innerHTML = newHeading;
+}
+
+function emailAddress() {
+    let email = prompt("What is your email adress?");
+    return true;
+}
+
+
+
 function clickButton () {
     let name = prompt("What is your name?");
-    let email = prompt("What is your email address?");
 
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = "To-do list for " + name +"!"
-   
+    if (emailAddress()) {
+    updateHeading(`To-do list for ${name}!`);
+   } else {
+    updateHeading(`Sorry <em>${name}</em> we cannot personalize your request!`);
+}
     }
 
     let personalizationButton = document.querySelector("button");
     personalizationButton.addEventListener("click", clickButton);
+    
     
